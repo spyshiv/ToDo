c894a2ba42e26df0cd145577ae0f018a32d48868
spyshiv <spyshiv@gmail.com>
2016-06-23 11:55:57 +0530

    Complete V1.0 Angular To do

--- static/js/todo_angular.js
+++ static/js/todo_angular.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-
     $("#add_input").hide();
+
     $( "#add_new_task" ).click(function() {
       $("#add_input").toggle(500);
     });
@@ -12,33 +12,36 @@ $(document).ready(function(){
 
 //Angular Functions
 function TodoCtrl($scope) {        
-        $scope.todos = [
-          {title:'Learn AngularJS', description: 'Learn Learn', timeline: '23/5/2016', completed: 'To do' },         
-          {title:'Learn Jquery', description: 'Learn Learn', timeline: '23/5/2016', completed: 'To do'},         
-        ];
+  $scope.todos = [
+    {title:'Learn AngularJS', description: 'AngularJS is what HTML would have been, had it been designed for building web-apps', timeline: '23/5/2016', completed: 'To do' },         
+    {title:'Learn Jquery', description: 'jQuery: The Write Less, Do More, JavaScript Library.', timeline: '23/5/2016', completed: 'To do'},         
+  ];
 
-        $scope.addTodo = function () {
-          $scope.todos.unshift({title:$scope.ToDoTitle, description:$scope.ToDoDesc,timeline:$scope.ToDoDate, completed: 'To do'});
-          $scope.ToDoTitle = '';
-          $scope.ToDoDesc = '';
-          $scope.ToDoDate = '';
-        };
+  $scope.addTodo = function () {
+    $scope.todos.unshift({title:$scope.ToDoTitle, description:$scope.ToDoDesc,timeline:$scope.ToDoDate, completed: 'To do'});
+    $scope.ToDoTitle = '';
+    $scope.ToDoDesc = '';
+    $scope.ToDoDate = '';
+  };
 
-        $scope.removeTodo = function(todotitle) {
-            for (var i =0; i < $scope.todos.length; i++)
-             if ($scope.todos[i].title === todotitle) {
-                $scope.todos.splice(i,1);
-                break;
-             }
-        }
-
-        $scope.completeTodo = function(todotitle) {
-            for (var i =0; i < $scope.todos.length; i++)
-             if ($scope.todos[i].title === todotitle) {
-                $scope.todos[i].completed = 'completed';
-                var element = document.getElementById(todotitle);
-                $(element).css("background-color", "green");
-                break;
-             }
-        }
+  $scope.removeTodo = function(todotitle) {
+    for (var i =0; i < $scope.todos.length; i++)
+      if ($scope.todos[i].title === todotitle) {
+        $scope.todos.splice(i,1);
+        break;
       }
+  }
+
+  $scope.completeTodo = function(todotitle) {
+    for (var i =0; i < $scope.todos.length; i++)
+      if ($scope.todos[i].title === todotitle) {
+        $scope.todos[i].completed = 'completed';
+        var element = document.getElementById(todotitle);
+        $(element).css({
+          "background-color" : "#2DAFA7",
+          "text-decoration" : "line-through"
+        });
+        break;
+      }
+  }
+}
