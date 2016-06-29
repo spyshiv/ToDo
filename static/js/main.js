b060b5b5ad2f0875144891c500df9a5f701acac6
spyshiv <spyshiv@gmail.com>
2016-06-30 00:24:42 +0530

    Fix floating background image on index page

--- static/js/main.js
+++ static/js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 	$("#main-container").mousemove(function(e){
           var pageX = e.pageX - ($(window).width() / 2);
           var pageY = e.pageY - ($(window).height() / 2);
-          var newvalueX = width * pageX * -1;
-          var newvalueY = height * pageY * -1;
+          var newvalueX = width * pageX * - 1 - 25;
+          var newvalueY = height * pageY * -1 - 50;
           $('#main-container').css("background-position", newvalueX+"px     "+newvalueY+"px");
 	});
 });
